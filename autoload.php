b86f09decee9cbd33610a5ac9bed61f2f1017f8c
fluxfw <fluxfw@fluxlabs.ch>
2022-06-14 12:42:46 +0200

    object

--- autoload.php
+++ autoload.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once __DIR__ . "/../../../UIComponent/UserInterfaceHook/flux_ilias_rest_helper_plugin/autoload.php";
+
+require_once __DIR__ . "/classes/class.ilflux_ilias_rest_leg_cron_helper_pluginPlugin.php";
--- classes/class.ilflux_ilias_rest_leg_cron_helper_pluginPlugin.php
+++ classes/class.ilflux_ilias_rest_leg_cron_helper_pluginPlugin.php
@@ -5,33 +5,32 @@ use FluxIliasRestApi\Libs\FluxIliasApi\Adapter\Api\IliasApi;
 class ilflux_ilias_rest_leg_cron_helper_pluginPlugin extends ilCronHookPlugin
 {
 
-    private IliasApi $ilias_api;
+    public static function getIliasApi() : IliasApi
+    {
+        require_once __DIR__ . "/../autoload.php";
+
+        return ilflux_ilias_rest_helper_pluginPlugin::getIliasApi();
+    }
 
 
     public function getCronJobInstance(/*string*/ $a_job_id) : ?ilCronJob
     {
-        return $this->ilias_api->getCronJob(
-            $a_job_id
-        );
+        return static::getIliasApi()
+            ->getCronJob(
+                $a_job_id
+            );
     }
 
 
     public function getCronJobInstances() : array
     {
-        return $this->ilias_api->getCronJobs();
+        return static::getIliasApi()
+            ->getCronJobs();
     }
 
 
     public function getPluginName() : string
     {
         return "flux_ilias_rest_leg_cron_helper_plugin";
-    }
-
-
-    protected function init() : void
-    {
-        require_once __DIR__ . "/../autoload.php";
-
-        $this->ilias_api = IliasApi::new();
     }
 }
